7edc4708696466135f1f8fc891d7fa904c37087e
“Shravaniii03” <PES2UG23CS552@pesu.pes.edu>
2026-04-02 10:33:09 +0530

    fix filler function calls to use 5 arguments for FUSE3 compatibility

--- src/dir_ops.c
+++ src/dir_ops.c
@@ -4,6 +4,8 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
 
 static int already_seen(char **seen, int count, const char *name) {
     for (int i = 0; i < count; i++) {
@@ -31,8 +33,8 @@ int unionfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     char *seen[1024];
     int count = 0;
 
-    filler(buf, ".", NULL, 0);
-    filler(buf, "..", NULL, 0);
+    filler(buf, ".", NULL, 0, 0);
+    filler(buf, "..", NULL, 0, 0);
 
     /* READ UPPER */
     dp = opendir(upper);
@@ -42,7 +44,7 @@ int unionfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
             if (is_whiteout_name(entry->d_name))
                 continue;
 
-            filler(buf, entry->d_name, NULL, 0);
+            filler(buf, entry->d_name, NULL, 0, 0);
 
             seen[count++] = strdup(entry->d_name);
         }
@@ -70,7 +72,7 @@ int unionfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
             if (access(wh_full, F_OK) == 0)
                 continue;
 
-            filler(buf, entry->d_name, NULL, 0);
+            filler(buf, entry->d_name, NULL, 0, 0);
         }
         closedir(dp);
     }
